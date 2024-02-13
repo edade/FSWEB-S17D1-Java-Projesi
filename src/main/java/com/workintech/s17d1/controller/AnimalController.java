8e2db997c1ca56874ab9abe40cb4627e23173d3e
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-02-13 03:21:07 +0300

    project done

---
+++ src/main/java/com/workintech/s17d1/controller/AnimalController.java
@@ -0,0 +1,45 @@
+package com.workintech.s17d1.controller;
+
+import com.workintech.s17d1.entity.Animal;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/workintech/animal")
+public class AnimalController {
+    private Map<Integer, Animal> animals;
+
+    @PostConstruct
+    public void init(){
+        animals = new HashMap<>();
+        animals.put(1, new Animal(1,"monkey"));
+    }
+
+    @GetMapping
+    public List<Animal> getAnimals(){
+        return new ArrayList<>(this.animals.values());
+    }
+    @GetMapping(path = "/{id}")
+    public Animal get(@PathVariable("id") Integer id){
+        return animals.get(id);
+    }
+    @PostMapping
+    public void addAnimal(@RequestBody Animal  animal){
+        this.animals.put(animal.getId(),animal);
+    }
+    @PutMapping(path = "/{id}")
+    public Animal updateAnimal(@PathVariable("id") Integer existingRecordId,@RequestBody Animal newAnimal){
+        this.animals.replace(existingRecordId,newAnimal);
+        return this.animals.get(existingRecordId);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteAnimal(@PathVariable Integer id){
+        this.animals.remove(id);
+    }
+}
